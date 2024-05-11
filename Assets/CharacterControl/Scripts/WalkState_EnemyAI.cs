32a54188909e2cde44376d7eef0c952b4d07c758
Sehoon Kim <hoona316@gmail.com>
2024-05-12 01:01:10 +0900

    Add Character Assets
    Add Medieval Warrior Asset for Player and AI Characters

---
+++ Assets/CharacterControl/Scripts/WalkState_EnemyAI.cs
@@ -0,0 +1,56 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkState_EnemyAI : StateMachineBehaviour
+{
+    private Transform playerLocation;
+    private NavMeshAgent navAgent;
+    private float attackRange = 2.7f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerLocation = GameObject.FindGameObjectWithTag("Player").transform;
+        navAgent = animator.GetComponent<NavMeshAgent>();
+        navAgent.SetDestination(playerLocation.position);
+
+        //change movement speed
+        navAgent.speed = 1.8f;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Look at the player direction
+        animator.transform.LookAt(playerLocation);
+
+        // Calculate the distance. If the distance is smaller than attackRange, change the state to "AttackReady"
+        float distance = Vector3.Distance(playerLocation.position, animator.transform.position);
+        if(distance < attackRange)
+        {
+            animator.SetBool("isReady", true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // When exiting the state, stop the character.
+        navAgent.SetDestination(navAgent.transform.position);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/CharacterControl/Scripts/WalkState_old.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkState : StateMachineBehaviour
+{
+    private float timer;
+    private float timerThreshold = 10;
+    private float chaseDistance = 8;
+    private List<Transform> wayPoints = new List<Transform> ();
+    private NavMeshAgent agent;
+    private Transform player;
+    private Vector3 playerDirection;
+    private Vector3 forwardDirection;
+    private float angle;
+    private float visionAngle = 120.0f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponent<NavMeshAgent>();
+        timer = 0;
+       
+        GameObject go = GameObject.FindGameObjectWithTag("Waypoint");
+        foreach (Transform t in go.transform)
+            wayPoints.Add(t);
+
+        agent.SetDestination(wayPoints[Random.Range(0, wayPoints.Count)].position);
+        agent.speed = 1.8f;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(agent.remainingDistance <= agent.stoppingDistance)
+            agent.SetDestination(wayPoints[Random.Range(0, wayPoints.Count)].position);
+
+        if (timer > 100000)
+            timer -= 100000;
+        timer += Time.deltaTime;
+        if (timer > timerThreshold)
+        {
+            animator.SetBool("isPatrol", false);
+            animator.SetBool("isOverwatch", true);
+        }
+
+        float distance = Vector3.Distance(player.position, animator.transform.position);
+        playerDirection = player.position - animator.transform.position;
+        forwardDirection = animator.transform.forward;
+        angle = Vector3.Angle(forwardDirection, playerDirection);
+        if (distance < chaseDistance && angle <= (visionAngle / 2.0f))
+        {
+            animator.SetBool("isChase", true);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(agent.transform.position);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
